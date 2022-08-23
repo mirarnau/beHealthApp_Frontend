d02c4a03fd5af2d9926d0ad8cb7084df3315c770
Arnau Mir Hurtado <arnaumirhurtado@Arnaus-MacBook-Pro.local>
2022-08-23 17:47:32 +0300

    First steps

---
+++ lib/Pages/mainPage.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+
+class MainPage extends StatefulWidget {
+  final int selectedIndex;
+  const MainPage({Key? key, required this.selectedIndex}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late int _selectedIndex = widget.selectedIndex;
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  late final screens = [Text(translate('nav_bar.appointments')), Text(translate('nav_bar.telemedicine')), Text(translate('nav_bar.new_appointment')), Text(translate('nav_bar.measurements')), Text(translate('nav_bar.my_health'))];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).cardColor,
+        title: Text(
+          translate('titles.beHealthApp'),
+          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: screens.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: translate('nav_bar.appointments'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone_iphone),
+            label: translate('nav_bar.telemedicine'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: translate('nav_bar.new_appointment'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: translate('nav_bar.measurements'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: translate('nav_bar.my_health'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        backgroundColor: Theme.of(context).backgroundColor,
+        unselectedItemColor: Theme.of(context).cardColor,
+        selectedItemColor: Theme.of(context).primaryColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,96 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unnecessary_const, prefer_const_constructors
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:medical_devices/Pages/mainPage.dart';
+
+void main() async {
+  await Settings.init(cacheProvider: SharePreferenceCache());
+
+  var delegate = await LocalizationDelegate.create(fallbackLocale: 'en', supportedLocales: ['en', 'es', 'ca']);
+
+  runApp(LocalizedApp(delegate, MyApp()));
 }
 
+//MyApp is the root widget
 class MyApp extends StatelessWidget {
+  //The fact that it's a statless widget ENABLES HOT RELOAD
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'beHealthApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    var localizationDelegate = LocalizedApp.of(context).delegate;
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return LocalizationProvider(
+      //MaterialApp is another widget, and it allows us to make the designs, it acts as a wrapper for the other widgets.
+      state: LocalizationProvider.of(context).state,
+      child: ValueChangeObserver<bool>(
+        //return ValueChangeObserver<bool>(
+        cacheKey: 'key-color-mode',
+        defaultValue: false,
+        builder: (_, isDarkMode, __) => MaterialApp(
+          title: 'beHealthApp',
+          localizationsDelegates: [GlobalMaterialLocalizations.delegate, GlobalWidgetsLocalizations.delegate, GlobalCupertinoLocalizations.delegate, localizationDelegate],
+          supportedLocales: localizationDelegate.supportedLocales,
+          locale: localizationDelegate.currentLocale,
+          theme: isDarkMode
+              ? ThemeData.dark().copyWith(
+                  primaryColor: Color.fromARGB(255, 213, 94, 85),
+                  scaffoldBackgroundColor: Color.fromARGB(255, 16, 16, 16),
+                  canvasColor: Color.fromARGB(255, 30, 30, 30),
+                  backgroundColor: const Color.fromARGB(255, 48, 48, 48),
+                  cardColor: Color.fromARGB(255, 149, 149, 149),
+                  focusColor: Color.fromARGB(255, 213, 94, 85),
+                  indicatorColor: Colors.white,
+                  shadowColor: Color.fromARGB(255, 104, 104, 104),
+                  hoverColor: Color.fromARGB(255, 96, 66, 64),
+                  highlightColor: Colors.white,
+                  hintColor: Color.fromARGB(255, 56, 55, 55),
+                  dialogBackgroundColor: Color.fromARGB(255, 226, 226, 226),
+                  dialogTheme: DialogTheme(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                  ))
+              : ThemeData.light().copyWith(
+                  primaryColor: Color.fromARGB(255, 5, 232, 185),
+                  scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+                  canvasColor: Color.fromARGB(255, 255, 255, 255),
+                  backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                  cardColor: Color.fromARGB(255, 30, 61, 72), //For the appbar and nav bar icons
+                  focusColor: Color.fromARGB(255, 255, 255, 255),
+                  indicatorColor: Color.fromARGB(255, 217, 105, 105),
+                  shadowColor: Color.fromARGB(255, 170, 170, 170),
+                  hoverColor: Color.fromARGB(255, 253, 225, 187),
+                  highlightColor: Colors.black,
+                  hintColor: Color.fromARGB(255, 219, 219, 219),
+                  dialogBackgroundColor: Color.fromARGB(255, 31, 31, 31),
+                  dialogTheme: DialogTheme(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                  )),
+          home: MainPage(
+            selectedIndex: 1,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+}
+
+class Palette {
+  static const MaterialColor kToDark = const MaterialColor(
+    0xff2B2B2B, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
+    const <int, Color>{
+      50: const Color(0xff4c4b4b), //10%
+      100: const Color(0x00444444), //20%
+      200: const Color(0xffa04332), //30%
+      300: const Color(0xff89392b), //40%
+      400: const Color(0xff733024), //50%
+      500: const Color(0xff5c261d), //60%
+      600: const Color(0xff451c16), //70%
+      700: const Color(0xff2e130e), //80%
+      800: const Color(0xff170907), //90%
+      900: const Color(0xff000000), //100%
+    },
+  );
 }
